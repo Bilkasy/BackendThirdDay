7fe8fa173fec1acf51a3fd5312f242be05bd1259
unknown <Bilkasy169@gmail.com>
2020-10-28 17:34:19 +0200

    First commit

---
+++ src/test/java/org/acme/resteasy/NativeStudentsExampleResourceIT.java
@@ -0,0 +1,9 @@
+package org.acme.resteasy;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeStudentsExampleResourceIT extends StudentsExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
